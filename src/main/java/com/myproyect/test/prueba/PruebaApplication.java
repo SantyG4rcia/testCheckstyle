51a1736a845b979134dbedd73d07aff9aca0be3f
santyDev <sg8140180@gmail.com>
2025-02-13 23:47:47 -0500

    Arreglando errores de husky

--- src/main/java/com/myproyect/test/prueba/PruebaApplication.java
+++ src/main/java/com/myproyect/test/prueba/PruebaApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PruebaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaApplication.class, args);
 	}
-
 }
 
